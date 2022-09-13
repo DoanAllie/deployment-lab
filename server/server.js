a0f73f40f5b97262df4c380cb9e52377850a95aa
DoanAllie <doan.allie@gmail.com>
2022-09-12 22:25:38 -0500

    third commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cors())
 
 çapp.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 const port = process.env.PORT || 4000
